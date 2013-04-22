9578e512b29615bc9f5dab1d1ee92baaad922121
Ray Chun <ray.chun@gmail.com>
2013-04-22 18:47:43 -0400

    Use later version of imagemagick in engineyard

--- cookbooks/emerge/recipes/default.rb
+++ cookbooks/emerge/recipes/default.rb
@@ -23,3 +23,12 @@
 #   version "2.7.6"
 #   action :install
 # end
+
+enable_package "media-gfx/imagemagick" do
+  version "6.4.9.2"
+end
+ 
+package "media-gfx/imagemagick" do
+    version "6.4.9.2"
+    action :install
+end
